ce95a98f205a1ee7dacce5704a4aa262fefab155
Paker Thomson <pdthomson1239@gmail.com>
2022-03-29 18:48:28 -0400

    "test:Add a test for a gameboard"

--- spec/welcome_message_spec.rb
+++ spec/welcome_message_spec.rb
@@ -8,8 +8,14 @@ RSpec.describe "Welcome message" do
     welcome = WelcomeMessage.new
     expect(welcome).to be_an_instance_of(WelcomeMessage)
   end
+
   it 'has a greeting' do
     welcome = WelcomeMessage.new
     expect(welcome.greeting).to eq('Welcome to Connect Four!')
+  end
+  
+  it 'has an empty gameboard' do
+    welcome = WelcomeMessage.new
+    expect(welcome.gameboard).to eq({})
   end
 end
