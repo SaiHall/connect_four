305917efc306d9c1939fba22c9f522b06c584395
Paker Thomson <pdthomson1239@gmail.com>
2022-03-30 13:46:38 -0400

    'feat:added an empty gameboard and a new gameboard method'

--- lib/welcome_message.rb
+++ lib/welcome_message.rb
@@ -5,4 +5,20 @@ class WelcomeMessage
     @greeting = 'Welcome to Connect Four!'
     @gameboard = {}
   end
+
+  def new_gameboard
+    @gameboard = {
+      row0: ["A", "B", "C", "D", "E", "F", "G"],
+      row6: [".", ".", ".", ".", ".", ".", "."],
+      row5: [".", ".", ".", ".", ".", ".", "."],
+      row4: [".", ".", ".", ".", ".", ".", "."],
+      row3: [".", ".", ".", ".", ".", ".", "."],
+      row2: [".", ".", ".", ".", ".", ".", "."],
+      row1: [".", ".", ".", ".", ".", ".", "."]}
+  end
 end
+
+# welcome = WelcomeMessage.new
+# puts welcome.greeting
+# welcome.new_gameboard
+# welcome.gameboard.map {|key,value|puts value.join}
