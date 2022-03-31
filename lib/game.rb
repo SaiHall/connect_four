ebf1aa2e19d00f02bda76d299ca2b8e3e47e94e1
Sarah Hall <swatson1994@yahoo.com>
2022-03-31 12:47:35 -0400

    "Feat: Add update method, and print method."

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Game
   attr_reader :gameboard
   def initialize
@@ -14,4 +16,27 @@ class Game
       row2: [".", ".", ".", ".", ".", ".", "."],
       row1: [".", ".", ".", ".", ".", ".", "."]}
   end
+
+  def update_board(index)
+    @gameboard.sort.find do |key,value|
+      if value[index] == '.'
+        value[index] = 'X'
+    # @gameboard = @gameboard.values[index].find do |element|
+    #   element == "."
+    #   element = 'X'
+    #   return @gameboard
+      end
+    end
+    print_board
+  end
+
+  def print_board #No method tests (CLI)
+    gameboard.map {|key,value|puts value.join}
+  end
+
 end
+
+# connect_four = Game.new
+# connect_four.reset_gameboard
+# connect_four.update_board(2)
+# connect_four.gameboard.map {|key,value|puts value.join}
