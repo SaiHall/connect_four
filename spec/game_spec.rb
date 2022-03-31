444f26437438096ccb469c25689659732c2c2db2
Sarah Hall <swatson1994@yahoo.com>
2022-03-30 20:19:37 -0400

    "Test: Moved gameboard tests to game, works better for it 2"

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -9,6 +9,22 @@ RSpec.describe 'Game' do
     expect(connect_four).to be_an_instance_of(Game)
   end
 
+  it 'has an empty gameboard' do
+    connect_four = Game.new
+    expect(connect_four.gameboard).to eq({})
+  end
 
-
+  it 'can reset the gameboard' do
+  connect_four = Game.new
+  connect_four.reset_gameboard
+  expect(connect_four.gameboard).to eq({
+    row0: ["A", "B", "C", "D", "E", "F", "G"],
+    row6: [".", ".", ".", ".", ".", ".", "."],
+    row5: [".", ".", ".", ".", ".", ".", "."],
+    row4: [".", ".", ".", ".", ".", ".", "."],
+    row3: [".", ".", ".", ".", ".", ".", "."],
+    row2: [".", ".", ".", ".", ".", ".", "."],
+    row1: [".", ".", ".", ".", ".", ".", "."]
+    })
+  end
 end
--- spec/welcome_message_spec.rb
+++ spec/welcome_message_spec.rb
@@ -13,23 +13,4 @@ RSpec.describe "Welcome message" do
     welcome = WelcomeMessage.new
     expect(welcome.greeting).to eq('Welcome to Connect Four!')
   end
-
-  it 'has an empty gameboard' do
-    welcome = WelcomeMessage.new
-    expect(welcome.gameboard).to eq({})
-  end
-
-  it 'has a fresh gameboard' do
-  welcome = WelcomeMessage.new
-  welcome.new_gameboard
-  expect(welcome.gameboard).to eq({
-    row0: ["A", "B", "C", "D", "E", "F", "G"],
-    row6: [".", ".", ".", ".", ".", ".", "."],
-    row5: [".", ".", ".", ".", ".", ".", "."],
-    row4: [".", ".", ".", ".", ".", ".", "."],
-    row3: [".", ".", ".", ".", ".", ".", "."],
-    row2: [".", ".", ".", ".", ".", ".", "."],
-    row1: [".", ".", ".", ".", ".", ".", "."]
-    })
-  end
 end
