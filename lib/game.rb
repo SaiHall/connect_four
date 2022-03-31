a5540397e8deddba1f99e55a0267ff9b4f3bdae8
Sarah Hall <swatson1994@yahoo.com>
2022-03-30 20:20:01 -0400

    "Feat: Moved gameboard methods to game class, for it 2"

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,17 @@
 class Game
+  attr_reader :gameboard
   def initialize
+    @gameboard = {}
+  end
+
+  def reset_gameboard
+    @gameboard = {
+      row0: ["A", "B", "C", "D", "E", "F", "G"],
+      row6: [".", ".", ".", ".", ".", ".", "."],
+      row5: [".", ".", ".", ".", ".", ".", "."],
+      row4: [".", ".", ".", ".", ".", ".", "."],
+      row3: [".", ".", ".", ".", ".", ".", "."],
+      row2: [".", ".", ".", ".", ".", ".", "."],
+      row1: [".", ".", ".", ".", ".", ".", "."]}
   end
 end
--- lib/welcome_message.rb
+++ lib/welcome_message.rb
@@ -1,19 +1,7 @@
 class WelcomeMessage
-  attr_reader :greeting, :gameboard
+  attr_reader :greeting
 
   def initialize
     @greeting = 'Welcome to Connect Four!'
-    @gameboard = {}
-  end
-
-  def new_gameboard
-    @gameboard = {
-      row0: ["A", "B", "C", "D", "E", "F", "G"],
-      row6: [".", ".", ".", ".", ".", ".", "."],
-      row5: [".", ".", ".", ".", ".", ".", "."],
-      row4: [".", ".", ".", ".", ".", ".", "."],
-      row3: [".", ".", ".", ".", ".", ".", "."],
-      row2: [".", ".", ".", ".", ".", ".", "."],
-      row1: [".", ".", ".", ".", ".", ".", "."]}
   end
 end
