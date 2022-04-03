4e5dc60b845564dac3cec24264fc0aa832e35b7a
Sarah Hall <swatson1994@yahoo.com>
2022-04-03 13:29:48 -0400

    "Test: Add tests for vertical wini conditions"

--- spec/play_spec.rb
+++ spec/play_spec.rb
@@ -6,12 +6,12 @@ require './lib/play'
 
 RSpec.describe 'Play' do
 
-  it "Exists" do
+  xit "Exists" do
     play = Play.new
     expect(play).to be_an_instance_of(Play)
   end
 
-  it "registers no horizontal win" do
+  xit "registers no horizontal win" do
     play = Play.new
     play.game.reset_gameboard
     gameboard = {
@@ -27,7 +27,7 @@ RSpec.describe 'Play' do
     expect(play.horizontal_win?).to eq(false)
   end
 
-  it "registers a horizontal win" do
+  xit "registers a horizontal win" do
     play = Play.new
     play.game.gameboard = {
        row0: ["A", "B", "C", "D", "E", "F", "G"],
@@ -65,7 +65,7 @@ RSpec.describe 'Play' do
     expect(play.horizontal_win?).to eq(true)
   end
 
-  it 'Registers horizonatal wins for O' do
+  xit 'Registers horizonatal wins for O' do
 
     play = Play.new
     play.game.gameboard = {
@@ -80,4 +80,47 @@ RSpec.describe 'Play' do
     expect(play.horizontal_win?).to eq(true)
   end
 
+  xit 'registers no vertical win' do
+    play = Play.new
+    play.game.reset_gameboard
+    gameboard = {
+      row0: ["A", "B", "C", "D", "E", "F", "G"],
+      row6: [".", ".", ".", ".", ".", ".", "."],
+      row5: [".", ".", ".", ".", ".", ".", "."],
+      row4: [".", ".", ".", ".", ".", ".", "."],
+      row3: [".", ".", ".", ".", ".", ".", "."],
+      row2: [".", ".", ".", ".", ".", ".", "."],
+      row1: [".", ".", ".", ".", ".", ".", "."]}
+
+
+    expect(play.vertical_win?).to eq(false)
+  end
+  xit 'registers a vertical win on the left' do
+    play = Play.new
+    play.game.gameboard = {
+      row0: ["A", "B", "C", "D", "E", "F", "G"],
+      row6: ["X", ".", ".", ".", ".", ".", "."],
+      row5: ["X", ".", ".", ".", ".", ".", "."],
+      row4: ["X", ".", ".", ".", ".", ".", "."],
+      row3: ["X", ".", ".", ".", ".", ".", "."],
+      row2: [".", ".", ".", ".", ".", ".", "."],
+      row1: [".", ".", ".", ".", ".", ".", "."]}
+
+
+    expect(play.vertical_win?).to eq(true)
+  end
+  it 'registers a vertical win on the right' do
+    play = Play.new
+    play.game.gameboard = {
+      row0: ["A", "B", "C", "D", "E", "F", "G"],
+      row6: [".", ".", ".", ".", ".", ".", "."],
+      row5: [".", ".", ".", ".", ".", ".", "."],
+      row4: [".", ".", ".", ".", ".", ".", "X"],
+      row3: [".", ".", ".", ".", ".", ".", "X"],
+      row2: [".", ".", ".", ".", ".", ".", "X"],
+      row1: [".", ".", ".", ".", ".", ".", "X"]}
+
+
+    expect(play.vertical_win?).to eq(true)
+  end
 end
